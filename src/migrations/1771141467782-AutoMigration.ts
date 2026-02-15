b840acf44efaa9da02b5a3c6f99491c8a7c5941c
Safidy <heriniaina.safidy.rabemorasata@gmail.com>
2026-02-15 10:44:54 +0300

    add project model

---
+++ src/migrations/1771141467782-AutoMigration.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AutoMigration1771141467782 implements MigrationInterface {
+    name = 'AutoMigration1771141467782'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "public"."protected_area" ALTER COLUMN "geometry" TYPE geometry`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "public"."protected_area" ALTER COLUMN "geometry" TYPE geometry(GEOMETRY,0)`);
+    }
+
+}
