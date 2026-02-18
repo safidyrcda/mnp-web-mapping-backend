43b7fdfff9f6d1ed90c6838fbf59da7cead69512
Safidy RABEMORASATA <safidy_rcda@mnparks.mg>
2026-02-18 08:18:51 +0300

    edit migration

--- src/migrations/1771328761594-AutoMigration.ts
+++ src/migrations/1771328761594-AutoMigration.ts
@@ -1,16 +1,23 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class AutoMigration1771328761594 implements MigrationInterface {
-    name = 'AutoMigration1771328761594'
+  name = 'AutoMigration1771328761594';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "public"."funder_funding" ADD "createdAt" TIMESTAMP NOT NULL DEFAULT now()`);
-        await queryRunner.query(`ALTER TABLE "public"."protected_area" ALTER COLUMN "geometry" TYPE geometry`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "public"."funder_funding" ADD "createdAt" TIMESTAMP NOT NULL DEFAULT now()`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "public"."protected_area" ALTER COLUMN "geometry" TYPE geometry`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "public"."protected_area" ALTER COLUMN "geometry" TYPE geometry(GEOMETRY,0)`);
-        await queryRunner.query(`ALTER TABLE "public"."funder_funding" DROP COLUMN "createdAt"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "public"."protected_area" ALTER COLUMN "geometry" TYPE geometry(GEOMETRY,0)`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "public"."funder_funding" DROP COLUMN "createdAt"`,
+    );
+  }
 }
